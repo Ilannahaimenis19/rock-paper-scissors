dba2c4dd8cb5110b2ff6b3fee99cd22cf41bc35b
IlannaHaimenis19 <188930393+Ilannahaimenis19@users.noreply.github.com>
2025-10-16 17:31:29 -0300

    Finish the logic and add a reset button

--- script.js
+++ script.js
@@ -7,102 +7,98 @@ function getComputerChoice() {
         return "paper";
     } else { 
         return "scissors";
-    } // categoriza os valores em 3
+    } 
+}
+  
+
+const displayPlayerScore = document.querySelector(".displayPlayerScore");
+const displayComputerScore = document.querySelector(".displayComputerScore")
+
+const button = document.querySelectorAll(".choice");
+
+const roundResultMessage = document.querySelector(".roundResultMessage");
+
+const displayPlayerChoice = document.querySelector(".displayPlayerChoice");
+const displayComputerChoice = document.querySelector(".displayComputerChoice");
+
+const result = document.querySelector(".result");
+
+const resultMessage = document.querySelector(".resultMessage");
+
+let playerScore = 0;
+let computerScore = 0;
+
+startGame()
+        
+function startGame() {
+    playerScore = 0;
+    computerScore = 0;
+
+    displayPlayerChoice.textContent = ""; 
+    displayComputerChoice.textContent = ""; 
+
+    displayPlayerScore.textContent = playerScore;
+    displayComputerScore.textContent = computerScore;
+
+    roundResultMessage.textContent = "";  
+    resultMessage.textContent = "";
+
+    setChoicesEnabled(true);
 }
 
-function getHumanChoice() {
-    let humanChoice = prompt("Input Rock, Paper or Scissors").toLowerCase() // pegar input do usuário
+
+function playRound(playerChoice, computerChoice) { 
+
+    if (playerChoice === "rock" && computerChoice === "scissors") {
+        displayPlayerScore.textContent = ++playerScore;
+        return roundResultMessage.textContent = "You win! Rock beats Scissors";
+    } else if (playerChoice === "rock" && computerChoice === "paper") {
+        displayComputerScore.textContent = ++computerScore;
+        return roundResultMessage.textContent = "You lose! Paper beats Rock";
+    } else if (playerChoice === "scissors" && computerChoice === "paper") {
+        displayPlayerScore.textContent = ++playerScore;
+        return roundResultMessage.textContent = "You win! Scissors beats Paper";
+    } else if (playerChoice === "scissors" && computerChoice === "rock") {
+        displayComputerScore.textContent = ++computerScore;
+        return roundResultMessage.textContent = "You lose! Rock beats Scissors";
+    } else if (playerChoice === "paper" && computerChoice === "rock") {
+        displayPlayerScore.textContent = ++playerScore;
+        return roundResultMessage.textContent = "You win! Paper beats Rock";
+    } else if (playerChoice === "paper" && computerChoice === "scissors") {
+        displayComputerScore.textContent = ++computerScore;
+        return roundResultMessage.textContent = "You lose! Scissors beats Paper"; 
+    } else return roundResultMessage.textContent = "It's a tie!"; 
+}
+
+
+button.forEach(button => 
+    button.addEventListener("click", () => {
+    const playerChoice = button.textContent.toLowerCase();
+    const computerChoice = getComputerChoice();
+        
+    if (playerScore < 5 && computerScore < 5) {
+        playRound(playerChoice, computerChoice);
+    } 
     
-    switch (humanChoice) {
-        case "rock":
-            return "rock";
-        case "paper":
-            return "paper";
-        case "scissors":
-            return "scissors";
+    if (playerScore === 5 || computerScore === 5) {
+        playerScore === 5 
+        ? resultMessage.textContent = "Congratulations! You won the game!"
+        : resultMessage.textContent = "Game over! The computer won the game!";    
+        setChoicesEnabled(false);
+        const resetButton = document.createElement("button");
+        resetButton.textContent = "Play Again";
+        result.appendChild(resetButton);
+        resetButton.addEventListener("click", () => {
+            startGame();
+            resetButton.remove();
+        })
     }
-        // retornar os valores rock, paper and scissors dependendo do que o usuário escrever.
+
+    displayPlayerChoice.textContent = playerChoice;
+    displayComputerChoice.textContent = computerChoice;
+    }));
+ 
+    function setChoicesEnabled(on) {
+    button.forEach(b => b.disabled = !on);
 }
 
-function playGame() {
-    //Definiu a função
-    function playRound(humanChoice, computerChoice) { // chamar os valores de computerChoice e humanChoice 
-        if (humanChoice === "rock" && computerChoice === "scissors") {
-            ++humanScore
-            return "You win! Rock beats Scissors";
-        } else if (humanChoice === "rock" && computerChoice === "paper") {
-            ++computerScore
-            return "You lose! Paper beats Rock";
-        } else if (humanChoice === "scissors" && computerChoice === "paper") {
-            ++humanScore
-            return "You win! Scissors beats Paper";
-        } else if (humanChoice === "scissors" && computerChoice === "rock") {
-            ++computerScore
-            return "You lose! Rock beats Scissors";
-        } else if (humanChoice === "paper" && computerChoice === "rock") {
-            ++humanScore
-            return "You win! Paper beats Rock";
-        } else if (humanChoice === "paper" && computerChoice === "scissors") {
-            ++computerScore
-            return "You lose! Scissors beats Paper"; // comparar os valores que são diferentes e retonar mensagem "venceu/perdeu"
-        } else return "It's a tie!"; // mensagem de "empate" se os valores forem os mesmos
-        }
-    
-    let humanScore = 0;
-    let computerScore = 0;
-
-    //chamou a função 1x 
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-    console.log("Round 1:",playRound(humanChoice, computerChoice));
-    console.log("Human chose:", humanChoice);
-    console.log("Computer chose:", computerChoice);
-    console.log("Human Score:", humanScore);
-    console.log("Computer Score:", computerScore);
-
-    //chamou a função 2x 
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-    console.log("Round 2:",playRound(humanChoice, computerChoice));
-    console.log("Human chose:", humanChoice);
-    console.log("Computer chose:", computerChoice);
-    console.log("Human Score:", humanScore);
-    console.log("Computer Score:", computerScore);
-
-    //chamou a função 3x 
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-    console.log("Round 3:",playRound(humanChoice, computerChoice));
-    console.log("Human chose:", humanChoice);
-    console.log("Computer chose:", computerChoice);
-    console.log("Human Score:", humanScore);
-    console.log("Computer Score:", computerScore);
-
-    //chamou a função 4x 
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-    console.log("Round 4:",playRound(humanChoice, computerChoice));
-    console.log("Human chose:", humanChoice);
-    console.log("Computer chose:", computerChoice);
-    console.log("Human Score:", humanScore);
-    console.log("Computer Score:", computerScore);
-
-    //chamou a função 5x 
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
-    console.log("Round 5:",playRound(humanChoice, computerChoice));
-    console.log("Human chose:", humanChoice);
-    console.log("Computer chose:", computerChoice);
-    console.log("Human Score:", humanScore);
-    console.log("Computer Score:", computerScore);
-
-    if (humanScore > computerScore) {
-        console.log("You win!");
-    } else if (humanScore < computerScore) {
-        console.log("You lose!")
-    }   else if (humanScore === computerScore) {
-        console.log("It's a tie!")
-    }
-}
-
-playGame()
